8c9be06fe93a85bb092a54d43ee4f2d2bd68ccab
eTarget1 <louisclifford10@yahoo.fr>
2019-03-27 13:28:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
+
+    $.get('index.html', function(response) {
+        // Here we are getting the element on the page with the id of sentences and
+        // inserting the response
+        $('#index').html(response);
+    });
+
+    $.get('this_doesnt_exist.html', function(data) {
+        // This will not be called because the .html file request doesn't exist
+        doSomethingGood();
+    }).fail(function(error) {
+        // This is called when an error occurs
+        console.log('Something went wrong: ' + error.statusText);
+    });
 });
+
